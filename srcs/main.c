4b2e6bf62686b94dce28a7750c8978652da34429
Gregoire Chamorel <gchamore@paul-f4Br3s5.clusters.42paris.fr>
2024-01-24 10:33:46 +0100

    okok

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: gchamore <gchamore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 12:36:25 by gchamore          #+#    #+#             */
-/*   Updated: 2024/01/24 10:19:05 by gchamore         ###   ########.fr       */
+/*   Updated: 2024/01/24 10:32:09 by gchamore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,13 @@ int main (int argc, char **argv)
 	head_b = ft_lstnew(NULL);
 	if (argc > 1)
 	{
-		
 		head_a = ft_parse_a(argc, argv, head_a);
 		if(!head_a)
-			return (ft_free_lst(head_a),ft_error(), 0);
+			return (ft_free_lst(head_a), ft_free_lst(head_b),ft_error(), 0);
 		// ft_push(&head_b, &head_a);
 		ft_print_piles(head_a, head_b);
 	}
+	ft_free_lst(head_a);
+	ft_free_lst(head_b);
 	return (0);
 }
