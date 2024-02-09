ab80ec78785ec99ec945de848116dcf71e579ef0
Gregoire Chamorel <gchamore@paul-f4Br3s5.clusters.42paris.fr>
2024-02-09 10:23:17 +0100

    OK NORMINETTE OK PROJET OK IL FAUTB ENLEVER LES DOSSIERS INUTILES + TESTEURS, RESTE A FAIRE : BONUS

--- srcs/utils/parsing_utils.c
+++ srcs/utils/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: gchamore <gchamore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 15:06:21 by gchamore          #+#    #+#             */
-/*   Updated: 2024/02/08 18:09:57 by gchamore         ###   ########.fr       */
+/*   Updated: 2024/02/09 09:55:08 by gchamore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,15 @@ int	ft_words_count(char *str)
 	int	length;
 	int	i;
 	int	check;
-	
+
 	length = 0;
 	i = 0;
 	check = 1;
 	while (str[i] != '\0')
 	{
-		
 		if (ft_is_delimiter(str[i]))
 			check = 1;
-		else if(check == 1)
+		else if (check == 1)
 		{
 			length++;
 			check = 0;
